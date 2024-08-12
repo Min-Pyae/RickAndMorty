d80c4e7c92d9f36bc38e1795fd657f6e42c49181
Min Pyae <minmp2002@gmail.com>
2024-08-12 18:07:37 +0100

    [ Clean ] Add Models

--- RickAndMorty/Models/RMEpisode.swift
+++ RickAndMorty/Models/RMEpisode.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+
+struct RMEpisode {
+    let id: Int
+    let name: String
+    let airData: String
+    let episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, episode, characters, url, created
+        case air_data = "airData"
+    }
+}
+
+
